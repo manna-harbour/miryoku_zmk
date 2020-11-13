fb82a37274bf3b013ec20d3693e51779547e7363
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2020-11-13 11:08:59 +1100

    Fix formatting

--- miryoku/ortho_4x12.h
+++ miryoku/ortho_4x12.h
@@ -6,20 +6,20 @@
 K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, \
 K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, \
 K20, K21, K22, K23, K24,           K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,           K35, K36, K37, N38, N39  \
+N30, N31, K32, K33, K34,           K35, K36, K37, N38, N39\
 )\
-K00  K01  K02  K03  K04  XXX  XXX  K05  K06  K07  K08  K09  \
-K10  K11  K12  K13  K14  XXX  XXX  K15  K16  K17  K18  K19  \
-K20  K21  K22  K23  K24  XXX  XXX  K25  K26  K27  K28  K29  \
+K00  K01  K02  K03  K04  XXX  XXX  K05  K06  K07  K08  K09 \
+K10  K11  K12  K13  K14  XXX  XXX  K15  K16  K17  K18  K19 \
+K20  K21  K22  K23  K24  XXX  XXX  K25  K26  K27  K28  K29 \
 XXX  XXX  K32  K33  K34  XXX  XXX  K35  K36  K37  XXX  XXX
 
 #define MIRYOKU_SUPERMAP(\
-K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, \
-K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, \
-K20, K21, K22, K23, K24,           K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,           K35, K36, K37, N38, N39  \
+K00,      K01,      K02, K03, K04,                     K05, K06, K07, K08,     K09, \
+K10,      K11,      K12, K13, K14,                     K15, K16, K17, K18,     K19, \
+K20,      K21,      K22, K23, K24,                     K25, K26, K27, K28,     K29, \
+N30,      N31,      K32, K33, K34,                     K35, K36, K37, N38,     N39  \
 )\
-K00  K01  K02  K03  K04  XXX  XXX  K05  K06  K07  K08  K09  \
-K10  K11  K12  K13  K14  XXX  XXX  K15  K16  K17  K18  K19  \
-K20  K21  K22  K23  K24  XXX  XXX  K25  K26  K27  K28  K29  \
-XXX  XXX  K32  K33  K34  XXX  XXX  K35  K36  K37  XXX  XXX
+K00       K01       K02  K03  K04  &kp NUM_8 &kp NUM_7 K05  K06  K07  K08      K09 \
+K10       K11       K12  K13  K14  &kp NUM_6 &kp NUM_5 K15  K16  K17  K18      K19 \
+K20       K21       K22  K23  K24  &kp NUM_4 &kp NUM_3 K25  K26  K27  K28      K29 \
+&kp LSHFT &kp LCTRL K32  K33  K34  &kp NUM_2 &kp NUM_1 K35  K36  K37  &kp LALT &kp LGUI
--- miryoku/split_3x6_3.h
+++ miryoku/split_3x6_3.h
@@ -6,20 +6,20 @@
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39  \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39\
 )\
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX  \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX  \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX  \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_SUPERMAP(\
-         K00,     K01,     K02,     K03,     K04,               K05,     K06,     K07,     K08,     K09, \
-         K10,     K11,     K12,     K13,     K14,               K15,     K16,     K17,     K18,     K19, \
-         K20,     K21,     K22,     K23,     K24,               K25,     K26,     K27,     K28,     K29, \
-         N30,     N31,     K32,     K33,     K34,               K35,     K36,     K37,     N38,     N39  \
+          K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+          K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+          K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+          N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39\
 )\
-&kp LALT K00      K01      K02      K03      K04                K05      K06      K07      K08      K09      &kp LGUI  \
-&kp LCTRL K10     K11      K12      K13      K14                K15      K16      K17      K18      K19      &kp LCTRL  \
-&kp LSHFT K20     K21      K22      K23      K24                K25      K26      K27      K28      K29      &kp LSHFT  \
-                           K32      K33      K34                K35      K36      K37
+&kp LALT  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 &kp LGUI \
+&kp LCTRL K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 &kp RCTRL \
+&kp LSHFT K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 &kp RSHFT \
+                    K32  K33  K34       K35  K36  K37
