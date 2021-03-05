c4394254f6c6288cb30a120e538bce52fde74592
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2021-03-05 17:23:25 +1100

    Change everything
    -add keymaps for all zmk splergos
    -add mappings
    -move babel to miryoku/miryoku-zmk-babel.org
    -generate only miryoku.dtsi from babel
    -move status to status.org
    -move docs to readme.org
    -change mapping macro names
    -add everything to build

--- miryoku/mapping/split_3x6_3.h
+++ miryoku/mapping/split_3x6_3.h
@@ -1,24 +1,24 @@
-// generated from miryoku_zmk.org  -*- buffer-read-only: t -*-
+// https://github.com/manna-harbour/miryoku-zmk/
 
 #define XXX &none
 
-#define MIRYOKU_SUBMAP(\
+#define MIRYOKU_MAPPING( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39\
-)\
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_SUPERMAP(\
+#define MIRYOKU_MAPPING_TAP( \
           K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
           K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
           K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-          N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39\
-)\
+          N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
 &kp LALT  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 &kp LGUI \
 &kp LCTRL K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 &kp RCTRL \
 &kp LSHFT K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 &kp RSHFT \
