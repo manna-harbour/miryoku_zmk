54ce4825fcc1c7db3f1b7f5c931595b798931672
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2021-05-17 12:15:02 +1000

    Add MIRYOKU_MAPPING_EXTENDED_THUMBS to miryoku/mapping/50-kyria.h

--- miryoku/mapping/50-kyria.h
+++ miryoku/mapping/50-kyria.h
@@ -1,7 +1,20 @@
 // https://github.com/manna-harbour/miryoku-zmk/
 
 #define XXX &none
-
+#define MIRYOKU_MAPPING(\
+     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
+)\
+XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
+XXX  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX  \
+XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
+               XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
+#define MIRYOKU_COMBO_TPSL 43 44
+#define MIRYOKU_COMBO_TPSR 45 46
+#if defined MIRYOKU_MAPPING_EXTENDED_THUMBS
+#else
 #define MIRYOKU_MAPPING(\
      K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
@@ -12,8 +25,9 @@ XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  
 XXX  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX  \
 XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
                XXX  K32  K33  K34  XXX       XXX  K35  K36  K37  XXX
+#define MIRYOKU_COMBO_TPSL 42 43
+#define MIRYOKU_COMBO_TPSR 46 47
+#endif
 
 #define MIRYOKU_MAPPING_TAP MIRYOKU_MAPPING // todo
 
-#define MIRYOKU_COMBO_TPSL 42 43
-#define MIRYOKU_COMBO_TPSR 48 49
