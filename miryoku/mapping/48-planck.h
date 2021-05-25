a8a1402782c748362640e9b131f5d66b30674635
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2021-05-25 10:48:35 +1000

    Add MIRYOKU_MAPPING_EXTENDED_THUMBS to 48-planck.h

--- miryoku/mapping/48-planck.h
+++ miryoku/mapping/48-planck.h
@@ -2,6 +2,18 @@
 
 #define XXX &none
 
+#if defined MIRYOKU_MAPPING_EXTENDED_THUMBS
+#define MIRYOKU_MAPPING(\
+K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,           K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,           K35, K36, K37, N38, N39 \
+) \
+K00  K01  K02  K03  K04  XXX  XXX  K05  K06  K07  K08  K09 \
+K10  K11  K12  K13  K14  XXX  XXX  K15  K16  K17  K18  K19 \
+K20  K21  K22  K23  K24  XXX  XXX  K25  K26  K27  K28  K29 \
+XXX  XXX  XXX  K32  K33  K34  K35  K36  K37  XXX  XXX  XXX
+#else
 #define MIRYOKU_MAPPING(\
 K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, \
 K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, \
@@ -12,14 +24,6 @@ K00  K01  K02  K03  K04  XXX  XXX  K05  K06  K07  K08  K09 \
 K10  K11  K12  K13  K14  XXX  XXX  K15  K16  K17  K18  K19 \
 K20  K21  K22  K23  K24  XXX  XXX  K25  K26  K27  K28  K29 \
 XXX  XXX  K32  K33  K34  XXX  XXX  K35  K36  K37  XXX  XXX
+#endif
 
-#define MIRYOKU_MAPPING_TAP( \
-K00,      K01,      K02, K03, K04,                     K05, K06, K07, K08,     K09, \
-K10,      K11,      K12, K13, K14,                     K15, K16, K17, K18,     K19, \
-K20,      K21,      K22, K23, K24,                     K25, K26, K27, K28,     K29, \
-N30,      N31,      K32, K33, K34,                     K35, K36, K37, N38,     N39  \
-) \
-K00       K01       K02  K03  K04  &kp NUM_8 &kp NUM_7 K05  K06  K07  K08      K09 \
-K10       K11       K12  K13  K14  &kp NUM_6 &kp NUM_5 K15  K16  K17  K18      K19 \
-K20       K21       K22  K23  K24  &kp NUM_4 &kp NUM_3 K25  K26  K27  K28      K29 \
-&kp LSHFT &kp LCTRL K32  K33  K34  &kp NUM_2 &kp NUM_1 K35  K36  K37  &kp LALT &kp LGUI
+#define MIRYOKU_MAPPING_TAP MIRYOKU_MAPPING // todo
