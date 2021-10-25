b2d25e0580c7c97180427ee22e2d48accbd7916f
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2021-10-25 17:20:05 +1100

    Add macros for all layer alternatives

---
+++ miryoku/babel/layer_names.h
@@ -0,0 +1,13 @@
+// Copyright 2021 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+// generated -*- buffer-read-only: t -*-
+
+#define BASE   0
+#define TAP    1
+#define BUTTON 2
+#define NAV    3
+#define MOUSE  4
+#define MEDIA  5
+#define NUM    6
+#define SYM    7
+#define FUN    8
