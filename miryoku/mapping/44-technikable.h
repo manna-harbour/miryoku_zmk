3ce97b8d382246c51fa284834d4c6f3ac43027d7
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2021-12-21 17:18:32 +1100

    Add technikable

---
+++ miryoku/mapping/44-technikable.h
@@ -0,0 +1,50 @@
+// Copyright 2021 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+#define XXX &none
+
+#if defined (MIRYOKU_MAPPING_EXTENDED_THUMBS)
+#define MIRYOKU_MAPPING(\
+K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,           K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,           K35, K36, K37, N38, N39 \
+) \
+K00  K01  K02  K03  K04  XXX  XXX  K05  K06  K07  K08  K09 \
+K10  K11  K12  K13  K14  XXX  XXX  K15  K16  K17  K18  K19 \
+K20  K21  K22  K23  K24  XXX  XXX  K25  K26  K27  K28  K29 \
+          XXX  K32  K33  K34  K35  K36  K37  XXX
+#elif defined (MIRYOKU_MAPPING_2X2U)
+#define MIRYOKU_MAPPING(\
+K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,           K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,           K35, K36, K37, N38, N39 \
+) \
+K00  K01  K02  K03  K04  XXX  XXX  K05  K06  K07  K08  K09 \
+K10  K11  K12  K13  K14  XXX  XXX  K15  K16  K17  K18  K19 \
+K20  K21  K22  K23  K24  XXX  XXX  K25  K26  K27  K28  K29 \
+          K32  K33  K34            K35  K36  K37
+#else
+#define MIRYOKU_MAPPING(\
+K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,           K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,           K35, K36, K37, N38, N39 \
+) \
+K00  K01  K02  K03  K04  XXX  XXX  K05  K06  K07  K08  K09 \
+K10  K11  K12  K13  K14  XXX  XXX  K15  K16  K17  K18  K19 \
+K20  K21  K22  K23  K24  XXX  XXX  K25  K26  K27  K28  K29 \
+          K32  K33  K34  XXX  XXX  K35  K36  K37
+#endif
+
+/ {
+  chosen {
+    zmk,matrix_transform =
+#if defined MIRYOKU_MAPPING_2X2U
+      &dual_2u_transform;
+#else
+      &ortho_transform;
+#endif
+  };
+};
