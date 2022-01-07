a2b6cc5882b29205b15628c2023c8500073c35bb
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2022-01-08 00:02:39 +1100

    Add bottom row combos

--- miryoku/miryoku.h
+++ miryoku/miryoku.h
@@ -1,4 +1,4 @@
-// Copyright 2021 Manna Harbour
+// Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
 #pragma once
@@ -41,44 +41,7 @@
 #endif
 
 #if defined (MIRYOKU_KLUDGE_MOUSEKEYSPR)
-  #define U_MOUSE_MOVE_MAX 1250
-  #define U_MOUSE_MOVE_EXPONENT 1
-  #define U_MOUSE_MOVE_TIME 1500
-  #define U_MOUSE_MOVE_DELAY 0
-  #define U_MOUSE_SCROLL_MAX 100
-  #define U_MOUSE_SCROLL_EXPONENT 1
-  #define U_MOUSE_SCROLL_TIME 5000
-  #define U_MOUSE_SCROLL_DELAY 0
-  #undef MOVE_UP
-  #undef MOVE_DOWN
-  #undef MOVE_LEFT
-  #undef MOVE_RIGHT
-  #define MOVE_UP MOVE_VERT(-U_MOUSE_MOVE_MAX)
-  #define MOVE_DOWN MOVE_VERT(U_MOUSE_MOVE_MAX)
-  #define MOVE_LEFT MOVE_HOR(-U_MOUSE_MOVE_MAX)
-  #define MOVE_RIGHT MOVE_HOR(U_MOUSE_MOVE_MAX)
-  #undef SCROLL_UP
-  #undef SCROLL_DOWN
-  #undef SCROLL_LEFT
-  #undef SCROLL_RIGHT
-  #define SCROLL_UP SCROLL_VERT(U_MOUSE_SCROLL_MAX)
-  #define SCROLL_DOWN SCROLL_VERT(-U_MOUSE_SCROLL_MAX)
-  #define SCROLL_LEFT SCROLL_HOR(-U_MOUSE_SCROLL_MAX)
-  #define SCROLL_RIGHT SCROLL_HOR(U_MOUSE_SCROLL_MAX)
-#endif
-
-#if defined (MIRYOKU_KLUDGE_MOUSEKEYSPR)
-  #define U_BTN1 &mkp MB1
-  #define U_BTN2 &mkp MB2
-  #define U_BTN3 &mkp MB3
-  #define U_MS_D &mmv MOVE_DOWN
-  #define U_MS_L &mmv MOVE_LEFT
-  #define U_MS_R &mmv MOVE_RIGHT
-  #define U_MS_U &mmv MOVE_UP
-  #define U_WH_D &mwh SCROLL_DOWN
-  #define U_WH_L &mwh SCROLL_LEFT
-  #define U_WH_R &mwh SCROLL_RIGHT
-  #define U_WH_U &mwh SCROLL_UP
+  #include "miryoku_kludge_mousekeyspr.h"
 #else
   #define U_BTN1 &kp KP_N5
   #define U_BTN2 U_NU
@@ -91,15 +54,4 @@
   #define U_WH_L U_NU
   #define U_WH_R U_NU
   #define U_WH_U U_NU
-#endif
-
-#if defined (MIRYOKU_KLUDGE_THUMBCOMBOS)
-  #define MIRYOKU_KLUDGE_THUMBCOMBOS_TERM 200
-  #define MIRYOKU_KLUDGE_THUMBCOMBOS_MACRO(layer, side, binding) \
-thumbcombos_ ## layer ## _ ## side { \
-  layers = <layer>; \
-  key-positions = <MIRYOKU_KLUDGE_THUMBCOMBOS_ ## side>; \
-  bindings = <binding>; \
-  timeout-ms = <MIRYOKU_KLUDGE_THUMBCOMBOS_TERM>; \
-};
 #endif
---
+++ miryoku/miryoku_kludge_mousekeyspr.h
@@ -0,0 +1,40 @@
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+#define U_MOUSE_MOVE_MAX 1250
+#define U_MOUSE_MOVE_EXPONENT 1
+#define U_MOUSE_MOVE_TIME 1500
+#define U_MOUSE_MOVE_DELAY 0
+#define U_MOUSE_SCROLL_MAX 100
+#define U_MOUSE_SCROLL_EXPONENT 1
+#define U_MOUSE_SCROLL_TIME 5000
+#define U_MOUSE_SCROLL_DELAY 0
+
+#undef MOVE_UP
+#undef MOVE_DOWN
+#undef MOVE_LEFT
+#undef MOVE_RIGHT
+#undef SCROLL_UP
+#undef SCROLL_DOWN
+#undef SCROLL_LEFT
+#undef SCROLL_RIGHT
+#define MOVE_UP MOVE_VERT(-U_MOUSE_MOVE_MAX)
+#define MOVE_DOWN MOVE_VERT(U_MOUSE_MOVE_MAX)
+#define MOVE_LEFT MOVE_HOR(-U_MOUSE_MOVE_MAX)
+#define MOVE_RIGHT MOVE_HOR(U_MOUSE_MOVE_MAX)
+#define SCROLL_UP SCROLL_VERT(U_MOUSE_SCROLL_MAX)
+#define SCROLL_DOWN SCROLL_VERT(-U_MOUSE_SCROLL_MAX)
+#define SCROLL_LEFT SCROLL_HOR(-U_MOUSE_SCROLL_MAX)
+#define SCROLL_RIGHT SCROLL_HOR(U_MOUSE_SCROLL_MAX)
+
+#define U_BTN1 &mkp MB1
+#define U_BTN2 &mkp MB2
+#define U_BTN3 &mkp MB3
+#define U_MS_D &mmv MOVE_DOWN
+#define U_MS_L &mmv MOVE_LEFT
+#define U_MS_R &mmv MOVE_RIGHT
+#define U_MS_U &mmv MOVE_UP
+#define U_WH_D &mwh SCROLL_DOWN
+#define U_WH_L &mwh SCROLL_LEFT
+#define U_WH_R &mwh SCROLL_RIGHT
+#define U_WH_U &mwh SCROLL_UP
