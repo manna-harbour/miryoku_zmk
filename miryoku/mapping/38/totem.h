a8dbe32fdefc215669238159d380f7fab1610ca7
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2022-11-11 14:31:02 +1100

    Fix totem

--- miryoku/mapping/38/totem.h
+++ miryoku/mapping/38/totem.h
@@ -11,9 +11,9 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-     K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  \
-K00  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K09 \
-     K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
+     K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
+     K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
+K00  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K09 \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 33 34
