40c8d6c325b13c304976cd7076af93ec06087d73
Kar Rui <karrui.lau@gmail.com>
2022-12-21 13:47:37 +0800

    feat: add custom config for skeletyl

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,14 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALTERNATIVES_SYM \
+&kp LS(GRAVE),     &kp LS(SEMI),      &kp LS(MINUS),     &kp LS(EQUAL),     &kp LS(BSLH),      U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp LS(NUM_1),     &kp LS(NUM_2),     &kp LS(NUM_3),     &kp LS(NUM_4),     &kp LS(NUM_5),     U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp LS(NUM_6),     &kp LS(NUM_7),     &kp LS(NUM_8),     &kp LS(NUM_9),     &kp LS(NUM_0),     U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp LS(LBKT),      &kp LS(RBKT),      &kp LS(DOT),       U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+#define MIRYOKU_ALTERNATIVES_NUM \
+&kp GRAVE,         &kp SEMI,          &kp MINUS,         &kp EQUAL,         &kp BSLH,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp NUM_4,         &kp NUM_5,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp NUM_6,         &kp NUM_7,         &kp NUM_8,         &kp NUM_9,         &kp NUM_0,         U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp LBKT,          &kp RBKT,          &kp DOT,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
