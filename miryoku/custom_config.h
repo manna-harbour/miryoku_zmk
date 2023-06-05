393f3a179c1159a9293cd2a860600f40eb1a56c1
Hidenari Kobayashi <hide1982aug@gmail.com>
2023-06-05 09:04:45 +0900

    update workflow

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_CLIPBOARD_WIN
+#define MIRYOKU_LAYERS_FLIP
+#define MIRYOKU_MAPPING_EXTENDED_THUMBS
