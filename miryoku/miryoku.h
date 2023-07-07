32042d8d8bae0f719365f6d381e5839b4d783862
Mattia Di Eleuterio <madiele92@gmail.com>
2023-07-07 21:39:31 +0200

    Incrase delay

--- miryoku/miryoku.h
+++ miryoku/miryoku.h
@@ -13,7 +13,7 @@
 #define U_NA &none // present but not available for use
 #define U_NU &none // available but not used
 
-#define U_TAPPING_TERM 200
+#define U_TAPPING_TERM 400
 
 #include "miryoku_clipboard.h"
 
