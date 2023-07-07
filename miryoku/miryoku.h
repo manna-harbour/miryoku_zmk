ed2828f3fca1a8be254fc9a150fee87ef8cffa55
Mattia Di Eleuterio <madiele92@gmail.com>
2023-07-07 21:22:44 +0200

    testd

--- miryoku/miryoku.h
+++ miryoku/miryoku.h
@@ -13,7 +13,7 @@
 #define U_NA &none // present but not available for use
 #define U_NU &none // available but not used
 
-#define U_TAPPING_TERM 1000
+#define U_TAPPING_TERM 200
 
 #include "miryoku_clipboard.h"
 
