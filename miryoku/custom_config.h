5b89c94eed7015b9972c38283f2ecd4c758386d3
Marco Peluso <marco@pelu.so>
2023-09-08 19:23:06 +0200

    Try to create own layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,8 +3,17 @@
 
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
+#define L_META 42
+
 #define MIRYOKU_LAYER_BASE \
-U_LT(U_NUM, Q),    &kp W,             &kp F,             &kp P,             &kp B,                         &kp J,             &kp L,             &kp U,             &kp Y,             U_LT(U_NAV, SQT),      \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,                         &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),         \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,                         &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH), \
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),            U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_LT(L_META, Q),    &kp W,             &kp F,               &kp P,               &kp B,                         &kp J,             &kp L,              &kp U,             &kp Y,            U_LT(U_NAV, SQT),      \
+U_MT(LGUI, A),      U_MT(LALT, R),     U_MT(LCTRL, S),      U_MT(LSHFT, T),      &kp G,                         &kp M,             U_MT(LSHFT, N),     U_MT(LCTRL, E),    U_MT(LALT, I),    U_MT(LGUI, O),         \
+U_LT(U_BUTTON, Z),  U_MT(RALT, X),     &kp C,               &kp D,               &kp V,                         &kp K,             &kp H,              &kp COMMA,         U_MT(RALT, DOT),  U_LT(U_BUTTON, SLASH), \
+U_NP,               U_NP,              U_LT(U_MEDIA, ESC),  U_LT(U_NAV, SPACE),  U_LT(U_MOUSE, TAB),            U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC),  U_LT(U_FUN, DEL),  U_NP,             U_NP
+
+#define MIRYOKU_LAYER_BASE \
+U_NU,  U_NU,  U_NU,  U_NU,  U_NU,      U_NU,  U_NU,  U_NU,  U_NU,  U_NU,  \
+U_NU,  U_NU,  U_NU,  U_NU,  U_NU,      U_NU,  &kp M,  U_NU,  U_NU,  U_NU,  \
+U_NU,  U_NU,  U_NU,  U_NU,  U_NU,      U_NU,  U_NU,  U_NU,  U_NU,  U_NU,  \
+U_NP,  U_NP,  U_NU,  U_NU,  U_NU,      U_NU,  U_NU,  U_NU,  U_NP,  U_NP
+
