95f31f354e3434632842044022bde8a2dc431753
Jack Bates <jack@nottheoilrig.com>
2023-10-08 13:57:58 -0700

    Replace reviung39 with reviung41_39

--- miryoku/mapping/41/reviung41.h
+++ miryoku/mapping/41/reviung41.h
@@ -14,7 +14,7 @@
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K36 \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K37 \
+K32  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K37 \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K35 \
                     K32  K33  K34  K36  K35
 
@@ -29,7 +29,7 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K35 \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 K33  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-K32  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+K32  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K37 \
 K34  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                     K34  K33  K35  K36  K37
 
