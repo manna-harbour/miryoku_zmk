7e46baf8b498a6919654ca151c7351ede6bbd8c3
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2023-12-09 21:27:03 +1100

    Update MIRYOKU_KLUDGE_MOUSEKEYSPR
    - Remove CONFIG_ZMK_MOUSE=y as no longer required
    - Use https://github.com/caksoylar/zmk/tree/feat/mouse-keys-3.2
    - Update behavior names, macros, headers, and docs

--- miryoku/miryoku_kludge_mousekeyspr.h
+++ miryoku/miryoku_kludge_mousekeyspr.h
@@ -3,33 +3,15 @@
 
 #pragma once
 
-#include <dt-bindings/zmk/mouse.h>
+#define ZMK_MOUSE_DEFAULT_MOVE_VAL 1250
+#define ZMK_MOUSE_DEFAULT_SCRL_VAL 100
 
-#define U_MOUSE_MOVE_MAX 1250
 #define U_MOUSE_MOVE_EXPONENT 1
 #define U_MOUSE_MOVE_TIME 1500
 #define U_MOUSE_MOVE_DELAY 0
-#define U_MOUSE_SCROLL_MAX 100
 #define U_MOUSE_SCROLL_EXPONENT 1
 #define U_MOUSE_SCROLL_TIME 5000
 #define U_MOUSE_SCROLL_DELAY 0
-
-#undef MOVE_UP
-#undef MOVE_DOWN
-#undef MOVE_LEFT
-#undef MOVE_RIGHT
-#undef SCROLL_UP
-#undef SCROLL_DOWN
-#undef SCROLL_LEFT
-#undef SCROLL_RIGHT
-#define MOVE_UP MOVE_VERT(-U_MOUSE_MOVE_MAX)
-#define MOVE_DOWN MOVE_VERT(U_MOUSE_MOVE_MAX)
-#define MOVE_LEFT MOVE_HOR(-U_MOUSE_MOVE_MAX)
-#define MOVE_RIGHT MOVE_HOR(U_MOUSE_MOVE_MAX)
-#define SCROLL_UP SCROLL_VERT(U_MOUSE_SCROLL_MAX)
-#define SCROLL_DOWN SCROLL_VERT(-U_MOUSE_SCROLL_MAX)
-#define SCROLL_LEFT SCROLL_HOR(-U_MOUSE_SCROLL_MAX)
-#define SCROLL_RIGHT SCROLL_HOR(U_MOUSE_SCROLL_MAX)
 
 #define U_BTN1 &mkp MB1
 #define U_BTN2 &mkp MB2
@@ -38,7 +20,7 @@
 #define U_MS_L &mmv MOVE_LEFT
 #define U_MS_R &mmv MOVE_RIGHT
 #define U_MS_U &mmv MOVE_UP
-#define U_WH_D &mwh SCROLL_DOWN
-#define U_WH_L &mwh SCROLL_LEFT
-#define U_WH_R &mwh SCROLL_RIGHT
-#define U_WH_U &mwh SCROLL_UP
+#define U_WH_D &msc SCRL_DOWN
+#define U_WH_L &msc SCRL_LEFT
+#define U_WH_R &msc SCRL_RIGHT
+#define U_WH_U &msc SCRL_UP
