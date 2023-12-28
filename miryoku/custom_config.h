06a802e0b7362c8b1eac290858dcd1fbb39e34b6
Jonas Holst Damtoft <Jomik@users.noreply.github.com>
2023-12-28 19:47:27 +0100

    substitute tap for game layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,18 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Substitute Tap layer for a gaming layer.
+#define MIRYOKU_LAYER_TAP \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+&kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
+&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              U_NP,              U_MT(LSHIFT, SPACE),TAB,              &kp RET,           &kp BSPC,          U_NP,              U_NP,              U_NP
+
+/ {
+    combos {
+        compatible = "zmk,combos";
+        MIRYOKU_KLUDGE_THUMBCOMBOS_MACRO(U_TAP, LEFT, U_MT(LALT, ESC))
+        MIRYOKU_KLUDGE_THUMBCOMBOS_MACRO(U_TAP, RIGHT, &u_to_U_BASE)
+    };
+};
+
