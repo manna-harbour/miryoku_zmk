d504c4e1ab83231c2b0d1ffe08bf625007bf017c
Jack Rickards <jack.rickards@hotmail.co.uk>
2024-01-07 22:36:00 +0000

    Update custom_config.h
    add custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_NAV=VI
+#define MIRYOKU_CLIPBOARD_MAC
+  
