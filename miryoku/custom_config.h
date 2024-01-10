892880f65ce834fa3400b627183eefcefd4945da
Jack Rickards <jack.rickards@hotmail.co.uk>
2024-01-10 00:04:09 +0000

    Update custom_config.h
    enable toprowcombos for testing

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
+#define MIRYOKU_KLUDGE_TOPROWCOMBOS
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_NAV_VI
 #define MIRYOKU_CLIPBOARD_MAC
