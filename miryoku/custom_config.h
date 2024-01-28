879552f84bfb7cb990760268599acb8c88b9d32a
Florian-Projects <43905823+Florian-Projects@users.noreply.github.com>
2024-01-28 17:38:17 +0100

    first attempt at unicode input

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,5 +1,11 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+#define HOST_OS 2
+#include "nodefreehelper.h"
+
+ZMK_UNICODE_PAIR( de_ae,   N0, N0,  E, N4,   N0, N0,  C, N4 )
+ZMK_UNICODE_PAIR( de_oe,   N0, N0,  F, N6,   N0, N0,  D, N6 )
+ZMK_UNICODE_PAIR( de_ue,   N0, N0,  F,  C,   N0, N0,  D,  C )
 
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
@@ -15,6 +21,17 @@
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp Q \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &u_bt_sel_4 \
+               K32  K33  K34       K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_BASE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &de_ae \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &de_oe \
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &de_ue \
                K32  K33  K34       K35  K36  K37
 #endif
 
