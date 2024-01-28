162b9199b41bcd264f0fc3dd35006d081e4105af
Florian-Projects <43905823+Florian-Projects@users.noreply.github.com>
2024-01-28 15:37:53 +0100

    add bluetooth 4

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,31 +3,20 @@
 
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
+#if defined (MIRYOKU_KEYBOARD_CORNE)
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_MEDIA( \
+#define MIRYOKU_LAYERMAPPING_MEDIA( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp Q \
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &u_bt_sel_4 \
                K32  K33  K34       K35  K36  K37
-
-#define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
-#define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
-#define MIRYOKU_KLUDGE_TOPROWCOMBOS_RIGHTINNERINDEX 7 8
-#define MIRYOKU_KLUDGE_TOPROWCOMBOS_RIGHTPINKIE 8 9
-
-#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_LEFTPINKIE 26 27
-#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_LEFTINNERINDEX 27 28
-#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_RIGHTINNERINDEX 31 32
-#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_RIGHTPINKIE 32 33
-
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 37 38
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 39 40
+#endif
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp D,             &kp R,             &kp W,             &kp B,             &kp J,             &kp F,             &kp U,             &kp P,             &kp SQT,              \
@@ -49,7 +38,7 @@ U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp 
 &kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
-#define MIRYOKU_ALTERNATIVES_MEDIA \
+#define MIRYOKU_LAYER_MEDIA \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RGB_TOG,         U_RGB_EFF,         U_RGB_HUI,         U_RGB_SAI,         U_RGB_BRI,         \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_EP_TOG,          &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
 U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &u_out_tog,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
