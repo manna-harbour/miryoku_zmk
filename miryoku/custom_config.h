9bc78c05732ec1cc4051722fdb87be54b48b6e56
Elliot Ruddock <elliot@ruddock.nz>
2024-06-01 22:16:25 +1200

    Update custom base layer definition

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,7 +4,7 @@
 
 #define MIRYOKU_CLIPBOARD_WIN
 
-#define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH \
+#define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 U_ET(U_UND, Z),    U_ET(U_CUT, X),    U_ET(U_COPY, C),   U_ET(U_PST, D),    &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
