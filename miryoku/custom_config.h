8db5dd8be3a5d4108e93f2d5038d2ad1e48b47a6
Elliot Ruddock <elliot@ruddock.nz>
2024-06-01 22:01:14 +1200

    Custom Colemak DH with edit tap

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,11 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+
+#define MIRYOKU_CLIPBOARD_WIN
+
+#define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_ET(U_UND, Z),    U_ET(U_CUT, X),    U_ET(U_COPY, C),   U_ET(U_PST, D),    &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC), U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPC),  U_LT(U_FUN, DEL),  U_NP,              U_NP
