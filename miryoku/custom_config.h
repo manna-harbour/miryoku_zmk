d69ce1f1f8db6db25506ed01be5684923c40e2d7
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2024-07-26 12:25:45 -0700

    feat(config): add custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,14 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MOUSE, ESC),U_LT(U_NAV, SPACE),U_LT(U_NUM, TAB),U_LT(U_FUN, RET),  U_LT(U_SYM, BSPC), U_LT(U_MEDIA, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_LAYER_NUM \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp MINUS,         &kp N4,            &kp N5,            &kp N6,            &kp PLUS,          \
+U_NA,              &kp RALT,          &u_to_U_NAV,       &u_to_U_NUM,       U_NA,              &kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,         \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp EQUAL,         &kp N0,            &kp DOT,           U_NP,              U_NP
