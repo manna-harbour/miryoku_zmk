fd1ff6c4dc80a39697369fade0f20ed774561e6f
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2024-07-26 13:49:53 -0700

    refactor(custom_config): replace colon with semi for sym layer in custom

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,3 +12,9 @@ U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp MINUS,         &kp N4,            &kp N5,            &kp N6,            &kp PLUS,          \
 U_NA,              &kp RALT,          &u_to_U_NAV,       &u_to_U_NUM,       U_NA,              &kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,         \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp EQUAL,         &kp N0,            &kp DOT,           U_NP,              U_NP
+
+#define MIRYOKU_LAYER_SYM \
+&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp SEMI,          &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
