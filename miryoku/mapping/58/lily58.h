cd39af11feae67f248843a30bf52ead87a2a8e93
Juan Toro <juan.diego.y.toro.cano.-nd@disney.com>
2024-10-06 16:42:39 -0500

    test

--- miryoku/mapping/58/lily58.h
+++ miryoku/mapping/58/lily58.h
@@ -4,6 +4,7 @@
 #if !defined (MIRYOKU_LAYOUTMAPPING_LILY58)
 
 #define XXX &none
+#define ACC &kp J
 
 #define MIRYOKU_LAYOUTMAPPING_LILY58( \
      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
@@ -13,7 +14,7 @@
 ) \
 XXX  XXX  XXX  XXX  XXX  XXX                 XXX  XXX  XXX  XXX  XXX  XXX \
 XXX  K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
+ACC  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  ACC \
 XXX  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
                XXX  K32  K33  K34       K35  K36  K37  XXX
 
