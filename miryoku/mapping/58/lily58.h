ecd03428823aa3697c8776852e240f6c6c40ce23
Juan Toro <juan.diego.y.toro.cano.-nd@disney.com>
2024-10-06 16:13:55 -0500

    Test

--- miryoku/mapping/58/lily58.h
+++ miryoku/mapping/58/lily58.h
@@ -7,14 +7,14 @@
 #define ACC &kp J
 
 #define MIRYOKU_LAYOUTMAPPING_LILY58( \
-     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
+      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
+      K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
+      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
+      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39, K40, K41 \
 ) \
 XXX  XXX  XXX  XXX  XXX  XXX                 XXX  XXX  XXX  XXX  XXX  XXX \
 XXX  K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
-ACC  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  ACC \
+K40  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  K41 \
 XXX  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
                XXX  K32  K33  K34       K35  K36  K37  XXX
 
