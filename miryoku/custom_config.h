e799d4590582bd22bbe8700225b3ebc7b215e790
David Gallmeier <dg@xounges.net>
2024-11-08 23:38:45 -0500

    update custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,19 +5,28 @@
 
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_CORNE( \
-     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+#define MIRYOKU_LAYOUTMAPPING_CORNE(\
+K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,                         K35, K36, K37, N38, N39 \
 ) \
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+               K32  K33  K34       K35  K36  K37
+
+#if defined MIRYOKU_KEYBOARD_CORNE
+#define MIRYOKU_LAYOUTMAPPING_BASE \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K40 \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 K38  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K39 \
                K32  K33  K34       K35  K36  K37
+#endif
 
 #define K38 &kp LSHFT
 #define K39 &kp RSHFT
+#define K40 &kp F5
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
@@ -33,5 +42,6 @@ K38  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K39 \
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 39 40
 
 #endif
+
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
