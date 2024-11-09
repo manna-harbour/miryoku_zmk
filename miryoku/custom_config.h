70250a263ae77b14172b7d76c3321b6f645906d7
David Gallmeier <dg@xounges.net>
2024-11-09 00:15:40 -0500

    add note
    [ci skip]

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -15,6 +15,8 @@ XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K38 \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K39 \
 K40  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K41 \
                K32  K33  K34       K35  K36  K37
+// to revise sym layer, see
+// https://github.com/mnivoliez/miryoku_zmk/blob/master/miryoku/custom_config.h
 
 #define K38 &kp F5
 #define K39 &kp F9
