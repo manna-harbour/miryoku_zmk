9cbb88f5a1a90274771ae86ddfa68c6bdf46d6b2
David Gallmeier <dg@xounges.net>
2024-11-12 13:28:42 -0500

    fix layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,8 +11,8 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K38 \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K39 \
+K42  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K38 \
+K43  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K39 \
 K40  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K41 \
                K32  K33  K34       K35  K36  K37
 // to revise sym layer, see
@@ -20,8 +20,12 @@ K40  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K41 \
 
 #define K38 &kp F5
 #define K39 &kp F9
+
 #define K40 &kp LSHFT
 #define K41 &kp RSHFT
+
+#define K42 &u_bt_sel_0
+#define K43 &u_bt_sel_0
 
 #define MIRYOKU_LAYER_SYM \
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          &kp F5,            &kp LS(F5),        &kp F9,            &kp F10,            &kp F11,            \
@@ -35,13 +39,11 @@ U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp 
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-K40  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K42 \
-K41  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K42 \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define K40 &u_bt_sel_0
-#define K41 &u_bt_sel_0
 #define K42 &kp F12
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
