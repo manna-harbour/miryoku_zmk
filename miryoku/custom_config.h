5a0bd423cbdb8a62d1520424d05fc758be850d86
David Gallmeier <dg@xounges.net>
2024-11-12 13:21:10 -0500

    add bluetooth keys

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -35,11 +35,13 @@ U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp 
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K42 \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+K40  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K42 \
+K41  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
+#define K40 &u_bt_sel_0
+#define K41 &u_bt_sel_0
 #define K42 &kp F12
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
