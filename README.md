249c2416c74aa1ed8e48b7b2e984368da827b4f4
David Gallmeier <dg@xounges.net>
2024-11-12 13:20:50 -0500

    Squashed commit of the following:
    commit b395ea5ff9145cb7676b6d9cffbdd20dac027ef9
    Author: David Gallmeier <dg@xounges.net>
    Date:   Tue Nov 12 11:16:51 2024 -0500
    
        enable oled

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 * https://github.com/manna-harbour/miryoku/discussions/158
 * https://github.com/manna-harbour/miryoku/discussions/293
+* https://github.com/manna-harbour/miryoku/discussions/85
--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -24,7 +24,7 @@ K40  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K41 \
 #define K41 &kp RSHFT
 
 #define MIRYOKU_LAYER_SYM \
-&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          &kp F5,            &u_to_U_BASE,      &kp F9,            &u_to_U_TAP,       U_BOOT,            \
+&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          &kp F5,            &kp LS(F5),        &kp F9,            &kp F10,            &kp F11,            \
 &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
@@ -36,13 +36,11 @@ U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp 
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K42 \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K43 \
-K40  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K44 \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define K42 &kp LS(F5)
-#define K43 &kp LC(F5)
-#define K44 &kp F12
+#define K42 &kp F12
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
