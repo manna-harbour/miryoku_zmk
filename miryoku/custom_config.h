bd32157cccf8886a49a9cb3d972c676b57b223b8
Stian H. Nesbø <stian.n@gmail.com>
2025-01-02 10:27:34 -0500

    Update custom_config.h
    Adding F11

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -27,7 +27,7 @@ U_NP,              U_NP,              &to U_BASE,        &kp SPC,           &non
 ) \
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &u_to_U_GAME \
 &caps_word  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &ext_power EP_ON \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+&kp F11  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_LAYER_LIST \
